af513f0bbca5b9fd8dcb086658a19564b14dcf79
DeCaelo <ludovic.cleuet@gmail.com>
2023-10-12 20:35:04 +0200

    afc 22 day 1 refacto

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 use std::fs;
 
 fn main() {
-    let content = fs::read_to_string("input").expect("file doesn't exist");
-    let counts = content
-        .split("\n\n")
-        .map(|chunk| -> usize { chunk.split("\n").map(|row| row.parse().unwrap_or(0)).sum() });
+    match fs::read_to_string("input") {
+        Ok(content) => {
+            let counts: Vec<usize> = content
+                .split_whitespace()
+                .map(|s| s.parse().unwrap_or(0))
+                .collect();
 
-    let mut v: Vec<_> = counts.collect();
-
-    v.sort();
-
-    let last_idx = v.len() - 1;
-
-    println!("the highest count is {}", v[last_idx])
+            match counts.iter().max() {
+                Some(max_count) => println!("The highest count is {}", max_count),
+                None => println!("No valid counts found in the input file."),
+            }
+        }
+        Err(err) => eprintln!("Error reading the input file: {}", err),
+    }
 }
+
+// Utilisation de split_whitespace au lieu de split("\n\n") et split("\n") :
+// Cela simplifie la séparation des nombres, car split_whitespace divise automatiquement
+// le texte en utilisant n'importe quel espace (y compris les sauts de ligne).
+
+// Utilisation de iter().max() :
+// Cela permet de trouver facilement la valeur maximale dans le vecteur counts sans trier le vecteur.
